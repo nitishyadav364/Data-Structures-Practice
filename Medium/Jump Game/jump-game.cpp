66b23735eba1cd06c21c6899326bf8fa8570909e
Nitish Kumar <97164239+nitishyadav364@users.noreply.github.com>
2024-01-21 17:11:38 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Jump Game/jump-game.cpp
@@ -0,0 +1,41 @@
+//{ Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+  public:
+    int canReach(int A[], int N) {
+        int final_position=N-1;
+        for(int position=final_position-1;position>=0;position--){
+            if(position+A[position]>=final_position){
+                final_position=position;
+            }
+        }
+        return final_position==0;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin>>N;
+        
+        int A[N];
+        
+        for(int i=0; i<N; i++)
+            cin>>A[i];
+
+        Solution ob;
+        cout << ob.canReach(A,N) << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
