4b6d409da592ca43b0401f3135b92ca25301ae5c
Nitish Kumar <97164239+nitishyadav364@users.noreply.github.com>
2024-05-13 22:39:40 +0530

    Updated Solution - GfG to GitHub

--- Medium/Heap Sort/heap-sort.cpp
+++ Medium/Heap Sort/heap-sort.cpp
@@ -7,28 +7,27 @@ using namespace std;
 // } Driver Code Ends
 // The functions should be written in a way that array become sorted 
 // in increasing order when heapSort() is called.
+
 class Solution
 {
     public:
     //Heapify function to maintain heap property.
     void heapify(int arr[], int n, int i)  
     {
-      // Your Code Here
-        int largest = i;
-        int left = 2 * i + 1;
-        int right = 2 * i + 2;
-      
-        if (left < n && arr[left] > arr[largest])
-          largest = left;
-      
-        if (right < n && arr[right] > arr[largest])
-          largest = right;
-      
-        // Swap and continue heapifying if root is not largest
-        if (largest != i) 
-        {
-          swap(arr[i], arr[largest]);
-          heapify(arr, n, largest);
+        int left=2*i+1; 
+        int right=2*i+2;
+        int largest=i;
+        if(left<n && arr[largest]<arr[left]){
+            largest=left;
+        }
+        
+        if(right<n&&arr[largest]<arr[right]){
+            largest=right;
+        }
+        
+        if(largest!=i){
+            swap(arr[largest],arr[i]);
+            heapify(arr,n,largest);
         }
     }
 
@@ -36,32 +35,23 @@ class Solution
     //Function to build a Heap from array.
     void buildHeap(int arr[], int n)  
     { 
-    // Your Code Here
-        for (int i = n / 2 - 1; i >= 0; i--)
-            heapify(arr, n, i);
+       for(int i=n/2;i>=0;i--){
+           heapify(arr,n,i);
+       } 
     }
 
     
     public:
     //Function to sort an array using Heap Sort.
     void heapSort(int arr[], int n)
-    {
-        //code here
+    {   
         buildHeap(arr,n);
-        // for(int i=0;i<n;i++)
-        //     cout<<arr[i]<<" ";
-        // cout<<endl;
-        for (int i = n - 1; i >= 0; i--)
-        {
-          swap(arr[0], arr[i]);
-      
-          // Heapify root element to get highest element at root again
-          heapify(arr, i, 0);
+        for(int i=n-1;i>=0;i--){
+            swap(arr[i],arr[0]);
+            heapify(arr,i,0);
         }
     }
 };
-
-
 
 
 
