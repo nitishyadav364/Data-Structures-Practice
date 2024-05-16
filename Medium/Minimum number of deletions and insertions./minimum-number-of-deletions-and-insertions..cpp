0b9c979a82bb47faddda7c55964db3f2ae73fd47
Nitish Kumar <97164239+nitishyadav364@users.noreply.github.com>
2024-05-16 22:06:45 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Minimum number of deletions and insertions./minimum-number-of-deletions-and-insertions..cpp
@@ -0,0 +1,50 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+		
+
+	public:
+	int minOperations(string str1, string str2) 
+	{ 
+	    int m=str1.length();
+	    int n=str2.length();
+	     vector<vector<int>> vec(m+1,vector<int>(n+1,0));
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(str1[i-1]==str2[j-1]){
+                    vec[i][j]=1+vec[i-1][j-1];
+                }
+                else{
+                    vec[i][j]=max(vec[i-1][j],vec[i][j-1]);
+                }
+            }
+        }
+        return m+n-2*vec[m][n];
+	} 
+};
+
+
+//{ Driver Code Starts.
+int main() 
+{
+   	
+   
+   	int t;
+    cin >> t;
+    while (t--)
+    {
+        string s1, s2;
+        cin >> s1 >> s2;
+
+	    Solution ob;
+	    cout << ob.minOperations(s1, s2) << "\n";
+	     
+    }
+    return 0;
+}
+
+
+// } Driver Code Ends
