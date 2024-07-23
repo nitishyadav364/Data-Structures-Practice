c9ff7015006dfe247383208125b1d9ee1407e277
Nitish Kumar <97164239+nitishyadav364@users.noreply.github.com>
2024-07-23 17:44:43 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Undirected Graph Cycle/undirected-graph-cycle.cpp
@@ -0,0 +1,62 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    // Function to detect cycle in an undirected graph.
+    bool solve(int i, vector<int> adj[], vector<int>& parent, vector<int>& visited) {
+    visited[i] = 1;
+    for (auto it : adj[i]) {
+        if (!visited[it]) {
+            parent[it] = i;
+            if (solve(it, adj, parent, visited)) {
+                return true;
+            }
+        } else if (parent[i] != it) {
+            return true;
+        }
+    }
+    return false;
+}
+
+bool isCycle(int V, vector<int> adj[]) {
+    vector<int> parent(V, -1);
+    vector<int> visited(V, 0);
+    
+    for (int i = 0; i < V; i++) {
+        if (!visited[i]) {
+            if (solve(i, adj, parent, visited)) {
+                return true;
+            }
+        }
+    }
+    return false;
+}
+};
+
+//{ Driver Code Starts.
+int main() {
+    int tc;
+    cin >> tc;
+    while (tc--) {
+        int V, E;
+        cin >> V >> E;
+        vector<int> adj[V];
+        for (int i = 0; i < E; i++) {
+            int u, v;
+            cin >> u >> v;
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+        Solution obj;
+        bool ans = obj.isCycle(V, adj);
+        if (ans)
+            cout << "1\n";
+        else
+            cout << "0\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
