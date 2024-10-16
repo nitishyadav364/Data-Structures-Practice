5750a326aee34201e4af36b16d53ae1bbdb9f4d6
Nitish Kumar <97164239+nitishyadav364@users.noreply.github.com>
2024-10-16 11:28:11 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
+++ Difficulty: Medium/Minimum Spanning Tree/minimum-spanning-tree.cpp
@@ -7,66 +7,31 @@ class Solution
 {
 	public:
 	//Function to find sum of weights of edges of the Minimum Spanning Tree.
-	int find(int x, vector<int>& parent) {
-    if (x == parent[x]) {
-        return x;
-    }
-    return parent[x] = find(parent[x], parent);
-}
-
-void unionByRank(int u, int v, vector<int>& parent, vector<int>& rank) {
-    int par_u = find(u, parent);
-    int par_v = find(v, parent);
-    
-    if (par_u == par_v) return;
-    if (rank[par_u] > rank[par_v]) {
-        parent[par_v] = par_u;
-    } else if (rank[par_u] < rank[par_v]) {
-        parent[par_u] = par_v;
-    } else {
-        parent[par_u] = par_v;
-        rank[par_v]++;
-    }
-}
-
-int spanningTree(int V, vector<vector<int>> adj[]) {
-    vector<pair<int, pair<int, int>>> vec;
-    for (int i = 0; i < V; i++) {
-        for (auto it : adj[i]) {
-            int v = it[0];
-            int wt = it[1];
-            vec.push_back({wt, {i, v}});
+    int spanningTree(int V, vector<vector<int>> adj[])
+    {
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> min_heap;
+        min_heap.push({0,0});
+        vector<bool> visited(V,false);
+        int sum=0;
+        while(!min_heap.empty()){
+            pair<int,int> temp=min_heap.top();
+            min_heap.pop();
+            int dist=temp.first;
+            int vertex=temp.second;
+            if(visited[vertex]){
+                continue;
+            }
+            visited[vertex]=true;
+            sum+=dist;
+            for(auto it:adj[vertex]){
+                if(!visited[it[0]]){
+                    min_heap.push({it[1],it[0]});
+                }
+            }
         }
+        return sum;
     }
-    
-    sort(vec.begin(), vec.end());
-    
-    vector<int> parent(V);
-    vector<int> rank(V, 0);
-    for (int i = 0; i < V; i++) {
-        parent[i] = i;
-    }
-    
-    int sum = 0;
-    for (auto it : vec) {
-        int wt = it.first;
-        int u = it.second.first;
-        int v = it.second.second;
-        
-        int par_u = find(u, parent);
-        int par_v = find(v, parent);
-        
-        if (par_u != par_v) {
-            sum += wt;
-            unionByRank(u, v, parent, rank);
-        }
-    }
-    
-    return sum;
-}
-
 };
-
 
 
 //{ Driver Code Starts.
